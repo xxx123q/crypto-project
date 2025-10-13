63b3021e54c04ed7565f7513aa09120d2c8547a8
Zi Lin <wenllnew41@gmail.com>
2025-10-13 17:47:16 +1100

    Set up table for crypto coin
    Allow data load from Coingecko api. Create basic table that contain name, symbol, price, 24h volum, market cap.

---
+++ src/app/page.jsx
@@ -0,0 +1,45 @@
+"use client";
+import { useEffect, useState } from "react";
+
+export default function Homepage() {
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    async function loadData() {
+      const res = await fetch(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1"
+      );
+      const data = await res.json();
+      setCoins(data);
+    }
+    loadData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Cryto List</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Symbol</th>
+            <th>Price</th>
+            <th>24h volume</th>
+            <th>Market Cap</th>
+          </tr>
+        </thead>
+        <tbody>
+          {coins.map((coin) => (
+            <tr key={coin.id}>
+              <td>{coin.name}</td>
+              <td>{coin.symbol}</td>
+              <td>${coin.current_price}</td>
+              <td>${coin.total_volume}</td>
+              <td>${coin.market_cap}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
