5c90a4030547187ea28986f71df4801b1eebf2e7
Zi Lin <wenllnew41@gmail.com>
2025-10-14 16:27:20 +1100

    Add loading state and pagination function
    Allow user to see loading when fetching data, allow use to got through pages if they want to see other page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,17 +4,51 @@ import Link from "next/link";
 
 export default function Homepage() {
   const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const perPage = 50;
+
+  useEffect(() => {
+    async function fetchTotalCoins() {
+      const res = await fetch("https://api.coingecko.com/api/v3/coins/list");
+      const coinList = await res.json();
+      const listLength = coinList.length;
+      setTotalPages(Math.ceil(listLength / perPage));
+    }
+    fetchTotalCoins();
+  }, []);
 
   useEffect(() => {
     async function loadData() {
+      setLoading(true);
+      const currency = "usd";
+      const order = "market_cap_desc";
+      const pageNumber = page;
       const res = await fetch(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1"
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=${order}&per_page=${perPage}&page=${pageNumber}`
       );
       const data = await res.json();
       setCoins(data);
+      setLoading(false);
     }
     loadData();
-  }, []);
+  }, [page]);
+
+  if (loading) return <p>Loading...</p>;
+
+  const getPageNumbers = () => {
+    const totalPageShowing = 5;
+    let start = Math.max(1, page - Math.floor(totalPageShowing / 2));
+    let end = start + totalPageShowing - 1;
+
+    if (end > totalPages) {
+      end = totalPages;
+      start = Math.max(1, end - totalPageShowing + 1);
+    }
+
+    return Array.from({ length: end - start + 1 }, (_, i) => start + i);
+  };
 
   return (
     <div>
@@ -43,6 +77,25 @@ export default function Homepage() {
           ))}
         </tbody>
       </table>
+      <button
+        onClick={() => setPage((p) => Math.max(p - 1, 1))}
+        disabled={page === 1}
+      >
+        ← Prev
+      </button>
+
+      {getPageNumbers().map((pageNum) => (
+        <button key={pageNum} onClick={() => setPage(pageNum)}>
+          {pageNum}
+        </button>
+      ))}
+
+      <button
+        onClick={() => setPage((p) => Math.min(p + 1, totalPages))}
+        disabled={page === totalPages}
+      >
+        Next →
+      </button>
     </div>
   );
 }
